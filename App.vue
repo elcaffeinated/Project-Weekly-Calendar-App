83dd230433328cf53588e61d434393365ff56ab3
elya <fionaeliahu@gmail.com>
2026-09-21 22:09:30 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <section class="section">
+      <div class="container">
+        <h1 class="title has-text-centered mb-6">Weekly Calendar App</h1>
+        <CalendarWeek />
+        <CalendarEntry />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import CalendarWeek from './app/components/CalendarWeek.vue';
+import CalendarEntry from './app/components/CalendarEntry.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CalendarWeek,
+    CalendarEntry
+  }
+};
+</script>
